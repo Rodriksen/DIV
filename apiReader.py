c7a80bbfe4acab30b36d4f84127f3be499018e24
rodrigodelera <rodrigodelera02@gmail.com>
2023-09-28 16:35:12 +0200

    error api

--- apiReader.py
+++ apiReader.py
@@ -1,9 +1,23 @@
 import requests
 import json
 
-edad = requests.get("https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/116?page=1")
-ipv = requests.get("https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/25171?date=20000901:20230928")
 
-print(edad.status_code)
+def fetch_data_from_api(api_url):
+    # Send a GET request to the API URL
+    response = requests.get(api_url)
 
-print(ipv.status_code)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON data if the API returns JSON
+        data = response.json()
+        return data
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        return None
+
+
+api_edad = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/116?page=1")
+api_ipv = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/25171?date=20000901:20230928")
+
+
+parsed_edad = json.loads(api_edad)
