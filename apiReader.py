69b09ee88ac367ea9dd510f8df96e00b27bca068
rodrigodelera <rodrigodelera02@gmail.com>
2023-10-04 16:20:51 +0200

    output to json

--- apiReader.py
+++ apiReader.py
@@ -12,12 +12,24 @@ def fetch_data_from_api(api_url):
         data = response.json()
         return data
     else:
-        print(f"Request failed with status code {response.status_code}")
+        print("Request failed with status code {response.status_code}")
         return None
 
 
-api_edad = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/116?page=1")
+file_path = "ipv.json"
+
+
+
 api_ipv = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/25171?date=20000901:20230928")
 
+# Write the data to the JSON file
+try:
+    with open(file_path, 'w') as json_file:
+        json.dump(api_ipv, json_file, indent=4)
+    print(f'Data has been successfully written to {file_path}')
+except Exception as e:
+    print(f'An error occurred: {str(e)}')
+
+api_edad = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/116?page=1")
 
 parsed_edad = json.loads(api_edad)
