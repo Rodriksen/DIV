3578c733fb2392cc1cbec9b15bb2a4ef160991b0
rodrigodelera <rodrigodelera02@gmail.com>
2023-10-18 17:57:20 +0200

    api edad funciona bien

--- apiReader.py
+++ apiReader.py
@@ -15,6 +15,7 @@ def fetch_data_from_api(api_url):
         print("Request failed with status code {response.status_code}")
         return None
 
+
 def createJson(file_path, data):
     try:
         with open(file_path, 'w') as json_file:
@@ -24,21 +25,28 @@ def createJson(file_path, data):
         print(f'An error occurred: {str(e)}')
 
 
-api_ipv = fetch_data_from_api("https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/IPV729?date=:")
-createJson("ipv.json", api_ipv)
-
 createPath = ""
 ageDict = dict()
-prov = open("provincias.txt", "r")
-series = open("seriesINE.txt", "r")
+prov = open("provincias.txt", "r", encoding="utf8")
+series = open("seriesINE.txt", "r", encoding="utf8")
+
 for i in range(1,52):
     provName = prov.readline()
+    print(provName)
     ageVector = []
-    for j in range(1,35):
-        series = series.readline(9)
-        createPath = "https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/" + series + "?date=20230101:20230106"
-        api_edad = fetch_data_from_api(createPath)
-        ageValDict = json.loads(api_edad)
-        ageValue = ageValDict["Valor"]
-        ageVector.append(ageValue)
 
+    for j in range(1, 35):
+        path = series.readline(9)
+        createPath = "https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/" + path + "?date=20230101:20230106"
+        api_edad = fetch_data_from_api(createPath)
+        midDict = api_edad["Data"]
+        ageValue = midDict[0]["Valor"]
+        ageVector.append(ageValue)
+        series.readline()
+
+    ageDict[provName] = ageVector
+
+
+createJson("age.json",ageDict)
+prov.close()
+series.close()
